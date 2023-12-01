c72586787df236b3c7440c04aefa1b144377e1da
Thomas Schmieck <mail@schmieck.de>
2023-12-01 20:40:11 +0100

    Test back navigation on production

--- src/routes/spell/[spellIndex]/+page.svelte
+++ src/routes/spell/[spellIndex]/+page.svelte
@@ -1,5 +1,35 @@
 <script>
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import { getSchoolColor } from "$lib/utils.js";
+
     export let data;
 </script>
 
-<h1>{data.selectedSpell.name}</h1>
+<div class="p-4 flex flex-col gap-4">
+    <div
+        class="min-h-[3rem] bg-[#fff]/[0.04] flex justify-between items-center py-4 px-6 rounded-lg text-sm hover:bg-[#fff]/[0.05]"
+    >
+        <button
+            type="button"
+            on:click={() => {
+                console.log(document.referrer);
+
+                if (document.referrer.indexOf($page.url.host) !== -1) {
+                    history.back();
+                } else {
+                    goto("/");
+                }
+            }}>Back to Spell List</button
+        >
+
+        <div class="flex items-center gap-2">
+            <h1>{data.selectedSpell.name}</h1>
+
+            <div
+                style="--color-school: {getSchoolColor(data.selectedSpell.school.index)}"
+                class="w-4 aspect-square bg-[var(--color-school)] rounded-sm"
+            ></div>
+        </div>
+    </div>
+</div>
