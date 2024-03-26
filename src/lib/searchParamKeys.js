aeef949e25926b1f499856752453fc5841720f90
Thomas Schmieck <mail@schmieck.de>
2024-03-26 01:40:09 +0100

    Add sort constants

--- src/lib/searchParamKeys.js
+++ src/lib/searchParamKeys.js
@@ -3,3 +3,7 @@ export const SPELL_SCHOOL_FILTER_KEY = "spell_school";
 export const SPELL_LEVEL_FILTER_KEY = "spell_level";
 export const SPELL_CLASS_FILTER_KEY = "spell_class";
 export const SPELL_SELECTED_FILTER_KEY = "spell_selected";
+export const SPELL_SORT_KEY = "spell_sort_by";
+
+/** @type {("name" | "level")[]} */
+export const SPELL_SORT_VALUES = ["name", "level"];
